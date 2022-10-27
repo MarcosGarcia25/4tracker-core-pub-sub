5ba70ed9ec03a3a951afae03e854e1d490ca977d
Marcos Garcia <marcos.mgarcia5@gmail.com>
2022-10-27 11:22:35 -0300

    alteração pubsub

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,24 @@
 import { subscribe } from './providers/pubsub';
 import handlerPositionTracker from './handler/position-tracker';
 import handlerTrackerCompany from './handler/tracker-company';
+import Redis from 'ioredis';
+import { CONFIGURE_REDIS } from './providers/constants/config-redis.const';
 
-subscribe('position-tracker', async (payload) => {
+subscribe(new Redis(CONFIGURE_REDIS), 'position-tracker', async (payload) => {
   console.log('position-tracker', payload);
   await handlerPositionTracker.execute(JSON.parse(payload));
 })
   .then(() => console.log('Subscribe Position Tracker running'))
   .catch(console.error);
 
-subscribe('tracker-company', async (payload) => {
+subscribe(new Redis(CONFIGURE_REDIS), 'tracker-company', async (payload) => {
   console.log('tracker-company', payload);
   await handlerTrackerCompany.execute(JSON.parse(payload));
 })
   .then(() => console.log('Subscribe Tracker Company running'))
   .catch(console.error);
 
-subscribe('remove-tracker-company', async (payload) => {
+subscribe(new Redis(CONFIGURE_REDIS), 'remove-tracker-company', async (payload) => {
   console.log('remove-tracker-company', payload);
   await handlerTrackerCompany.remove(JSON.parse(payload));
 })
--- src/providers/pubsub.ts
+++ src/providers/pubsub.ts
@@ -2,24 +2,15 @@ import Redis from 'ioredis';
 import { CONFIGURE_REDIS } from './constants/config-redis.const';
 
 const clientPublish = new Redis(CONFIGURE_REDIS);
-const clientSubscribe = new Redis(CONFIGURE_REDIS);
 
 export async function publish(channel, value) {
   return await clientPublish.publish(channel, JSON.stringify(value));
 }
 
-export async function subscribe(channel, callback) {
+export async function subscribe(clientSubscribe, channel, callback) {
   clientSubscribe.on('message', (channel, message) => {
     callback(message);
   });
 
   clientSubscribe.subscribe(channel);
 }
-
-clientPublish.on('error', (error) => {
-  console.error(error);
-});
-
-clientSubscribe.on('error', (error) => {
-  console.error(error);
-});
