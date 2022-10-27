92a3b25687e891c857063044f1416860ce95b1cb
Marcos Garcia <marcos.mgarcia5@gmail.com>
2022-10-27 11:04:43 -0300

    add logs

--- src/app.ts
+++ src/app.ts
@@ -3,18 +3,21 @@ import handlerPositionTracker from './handler/position-tracker';
 import handlerTrackerCompany from './handler/tracker-company';
 
 subscribe('position-tracker', async (payload) => {
+  console.log('position-tracker', payload);
   await handlerPositionTracker.execute(JSON.parse(payload));
 })
   .then(() => console.log('Subscribe Position Tracker running'))
   .catch(console.error);
 
 subscribe('tracker-company', async (payload) => {
+  console.log('tracker-company', payload);
   await handlerTrackerCompany.execute(JSON.parse(payload));
 })
   .then(() => console.log('Subscribe Tracker Company running'))
   .catch(console.error);
 
 subscribe('remove-tracker-company', async (payload) => {
+  console.log('remove-tracker-company', payload);
   await handlerTrackerCompany.remove(JSON.parse(payload));
 })
   .then(() => console.log('Subscribe Remove Tracker Company running'))
