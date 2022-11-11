b9d561cc39ab73a03a31bfb5f887d734b48f14e9
Marcos Garcia <marcos.mgarcia5@gmail.com>
2022-11-11 11:17:26 -0300

    alteração host redis para pubsub

--- src/repositories/PositionRepository.ts
+++ src/repositories/PositionRepository.ts
@@ -1,43 +1,57 @@
 import { PositionModel } from '../entities/position';
+import { CacheProvider } from '../providers/cache';
 import { IPositionRepository } from './interfaces/IPositionRepository';
 
 export class PositionRepository implements IPositionRepository {
   async findByCompany(companyId: string) {
-    const positions = await PositionModel.aggregate([
-      {
-        $match: {
-          companyId,
-        },
-      },
-      {
-        $group: {
-          id: { $last: '$_id' },
-          latitude: { $last: '$latitude' },
-          longitude: { $last: '$longitude' },
-          trackerId: { $last: '$trackerId' },
-          _id: {
-            vehicleId: '$vehicleId',
-            companyId: '$companyId',
+    let positions = null;
+    const keyCache = `positions:company:${companyId}`;
+    
+    const positionsCache = await CacheProvider.get(keyCache);
+
+    if (positionsCache) {
+      positions = JSON.parse(positionsCache);
+    } else {
+      positions = await PositionModel.aggregate([
+        {
+          $match: {
+            companyId,
           },
-          speed: { $last: '$speed' },
-          vehicle: { $last: '$vehicle' },
-          tracker: { $last: '$tracker' },
-          createdAt: { $last: '$createdAt' },
-          userId: { $last: '$userId' },
-          journeyId: { $last: '$journeyId' },
-          user: { $last: '$user' },
-          journey: { $last: '$journey' },
         },
-      },
-    ]);
+        {
+          $group: {
+            id: { $last: '$_id' },
+            latitude: { $last: '$latitude' },
+            longitude: { $last: '$longitude' },
+            trackerId: { $last: '$trackerId' },
+            _id: {
+              vehicleId: '$vehicleId',
+              companyId: '$companyId',
+            },
+            speed: { $last: '$speed' },
+            vehicle: { $last: '$vehicle' },
+            tracker: { $last: '$tracker' },
+            createdAt: { $last: '$createdAt' },
+            userId: { $last: '$userId' },
+            journeyId: { $last: '$journeyId' },
+            user: { $last: '$user' },
+            journey: { $last: '$journey' },
+          },
+        },
+      ]);
 
-    return positions.map((position) => {
-      position.vehicleId = position['_id'].vehicleId;
-      position.companyId = position['_id'].companyId;
+      positions = positions.map((position) => {
+        position.vehicleId = position['_id'].vehicleId;
+        position.companyId = position['_id'].companyId;
 
-      delete position['_id'];
+        delete position['_id'];
 
-      return position;
-    });
+        return position;
+      });
+
+      await CacheProvider.setEx(keyCache, 60, JSON.stringify(positions));
+    }
+
+    return positions;
   }
 }
