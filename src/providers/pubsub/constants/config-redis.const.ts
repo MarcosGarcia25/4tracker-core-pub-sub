e990e76b3186e1c16043ade1c04d45d8cd112b36
Marcos Garcia <marcos.mgarcia5@gmail.com>
2022-11-23 11:15:07 -0300

    connectTimeout redis

--- src/providers/pubsub/constants/config-redis.const.ts
+++ src/providers/pubsub/constants/config-redis.const.ts
@@ -3,10 +3,12 @@ import { RedisOptions } from 'ioredis';
 export const CONFIGURE_REDIS: RedisOptions = {
   port: Number(process.env.REDIS_PORT),
   host: process.env.REDIS_HOST,
-  db: Number(process.env.REDIS_DB)
+  db: Number(process.env.REDIS_DB),
+  connectTimeout: 2000,
 };
 
 export const CONFIGURE_REDIS_PUBSUB: RedisOptions = {
   port: Number(process.env.REDIS_PUBSUB_PORT),
   host: process.env.REDIS_PUBSUB_HOST,
+  connectTimeout: 2000,
 };
