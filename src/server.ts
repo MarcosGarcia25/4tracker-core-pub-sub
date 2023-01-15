924c2ec95087738dd8fa17885da537a7cf4c171e
Marcos Garcia <marcos.mgarcia5@gmail.com>
2023-01-15 00:51:28 -0300

    edição de devices

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import App from './app';
 import { JourneyService } from './handler/journey';
 import { PositionTrackerService } from './handler/position-tracker';
 import { TrackerCompanyService } from './handler/tracker-company';
+import { TrackerDeviceService } from './handler/tracker-device';
 import { CacheProvider } from './providers/cache';
 import { SubscribeService } from './subscribe-service';
 
@@ -12,6 +13,7 @@ const app = new App(
     new TrackerCompanyService(new CacheProvider()),
     new PositionTrackerService(new CacheProvider()),
     new JourneyService(),
+    new TrackerDeviceService(),
   ),
 );
 app.initSubscribe();
