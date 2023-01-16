e49a1897c38fe6bc81e186d6727184173c009262
Marcos Garcia <marcos.mgarcia5@gmail.com>
2023-01-15 23:08:46 -0300

    criação serviço de envio de sms

--- src/handler/tracker-device/index.ts
+++ src/handler/tracker-device/index.ts
@@ -4,20 +4,29 @@ import { ITrackerDeviceService } from './interfaces/ITrackerDeviceService.interf
 
 export class TrackerDeviceService implements ITrackerDeviceService {
   async update(payload: IDevice): Promise<void> {
-    const trackers = await TrackerModel.find({ 'tracker.device.id': payload.id });
+    // const trackers = await TrackerModel.find({ 'tracker.device.id': payload.id });
 
-    for (let tracker of trackers) {
-      const newTracker = tracker.tracker;
-      newTracker.device = payload;
-
-      await TrackerModel.updateMany(
-        { trackerId: tracker.trackerId },
-        {
-          $set: {
-            tracker: newTracker,
-          },
+    await TrackerModel.updateMany(
+      { 'tracker.device.id': payload.id },
+      {
+        $set: {
+          'tracker.device': payload,
         },
-      );
-    }
+      },
+    );
+
+    // for (let tracker of trackers) {
+    //   const newTracker = tracker.tracker;
+    //   newTracker.device = payload;
+
+    //   await TrackerModel.updateMany(
+    //     { trackerId: tracker.trackerId },
+    //     {
+    //       $set: {
+    //         tracker: newTracker,
+    //       },
+    //     },
+    //   );
+    // }
   }
 }
