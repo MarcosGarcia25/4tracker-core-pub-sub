e2c4bd08a42d7ad731e9a6f8e526e792fbe34f2e
Marcos Garcia <marcos.mgarcia5@gmail.com>
2023-01-31 19:40:30 -0300

    criação endpoir para pegar a rota de um veiculo por data

--- src/http-server/domain/position/index.ts
+++ src/http-server/domain/position/index.ts
@@ -5,5 +5,5 @@ import { CacheProvider } from '../../../providers/cache';
 import { DateUtilService } from '../../../shared/utils/date-util.service';
 
 export const positionController = new PositionController(
-  new PositionService(new PositionRepository(new CacheProvider()), new DateUtilService()),
+  new PositionService(new PositionRepository(new CacheProvider())),
 );
--- src/http-server/services/Position.service.ts
+++ src/http-server/services/Position.service.ts
@@ -1,13 +1,10 @@
 import { BaseService } from '../../base/BaseService';
 import { IPositionRepository } from '../../repositories/interfaces/IPositionRepository';
-import { ErrorCode } from '../../shared/enum/ErrorCode.enum';
-import { HttpStatus } from '../../shared/enum/HttpStatus.enum';
-import { IDateUtilService } from '../../shared/utils/interfaces/IDateUtilService.interface';
 import { IDriverByCompanyAndCoordinate, IFindByVehicleAndPeriod } from '../domain/position/interfaces';
 import { IPositionService } from './interfaces/IPositionService.interface';
 
 export class PositionService extends BaseService implements IPositionService {
-  constructor(private positionRepository: IPositionRepository, private dateUtilService: IDateUtilService) {
+  constructor(private positionRepository: IPositionRepository) {
     super();
   }
 
@@ -24,19 +21,6 @@ export class PositionService extends BaseService implements IPositionService {
   }
 
   async getAllByVehicleAndPeriod(payload: IFindByVehicleAndPeriod) {
-    const limitHour = 24;
-    if (this.dateUtilService.createDate(payload.endDate).diff(payload.startDate) > 1000 * 60 * 60 * limitHour) {
-      throw this.error(
-        HttpStatus.BAD_REQUEST,
-        `A data e hora deve estar dentro de ${limitHour} horas`,
-        ErrorCode.ERROR_FIELD,
-      );
-    }
-
-    if (this.dateUtilService.createDate(payload.endDate).diff(payload.startDate) < 0) {
-      throw this.error(HttpStatus.BAD_REQUEST, `A data inicial deve ser menor que a final`, ErrorCode.ERROR_FIELD);
-    }
-
     return await this.positionRepository.findByVehicleAndPeriod(payload);
   }
 }
